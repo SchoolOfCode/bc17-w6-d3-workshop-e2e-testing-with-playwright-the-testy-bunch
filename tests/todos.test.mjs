f7ccfcc737936be48317f13ed35dd0ad922566be
Faisal Ahmed <github@fais.al>
2024-08-08 12:29:59 +0100

    find text box task done

--- tests/todos.test.mjs
+++ tests/todos.test.mjs
@@ -3,3 +3,8 @@ import { test, expect } from '@playwright/test';
 test("practice test should pass", async ({ page }) => {
 await page.goto('http://localhost:10001');
 });
+
+test("find the text box", async ({ page }) => {
+  await page.goto('http://localhost:10001');
+  await page.getByRole("textbox");
+})
